c1dd66f0349ff1201fc1cc59809c61c38c4deda7
uğur cengiz <ugur_nobre@hotmail.com>
2023-02-23 18:29:37 +0300

    23.02.23

---
+++ src/app/ui/components/auth/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { ValidInputDirective } from 'src/app/common/directives/valid-input.directive';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule,RouterModule,FormsModule,ValidInputDirective],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  
+  login(form:NgForm){
+    if(form.valid){
+      console.log(form.value)
+    } 
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { importProvidersFrom } from "@angular/core";
+import { bootstrapApplication, BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { AppComponent } from "./app/app.component";
+import { AuthGuard } from "./app/ui/components/auth/guards/auth.guard";
+import {provideHttpClient} from "@angular/common/http"
+bootstrapApplication(AppComponent,{
+  providers:[
+    provideHttpClient(),
+    importProvidersFrom(
+      BrowserModule,
+      RouterModule.forRoot([
+        {
+          path:"",
+          loadComponent :() =>import("./app/ui/components/layouts/layouts.component").then(c=>c.LayoutsComponent),
+          canActivateChild:[AuthGuard],
+          children :[
+            {
+              path:"",
+              loadComponent:() => import("./app/ui/components/blank/blank.component").then(c=>c.BlankComponent)
+            }
+          ]             
+          
+        },
+        {
+          path:"login",
+          loadComponent:()=> import("./app/ui/components/auth/login/login.component").then(p=>p.LoginComponent)
+        }
 
-import { AppModule } from './app/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+      ])
+    )
+  ]
+})
