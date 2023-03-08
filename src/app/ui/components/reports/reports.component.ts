81f8f10bdc4701679259ce6aed3437e87d3941a5
uğur cengiz <ugur_nobre@hotmail.com>
2023-03-08 19:03:34 +0300

    08.03.23

--- src/app/ui/components/reports/reports.component.ts
+++ src/app/ui/components/reports/reports.component.ts
@@ -5,6 +5,7 @@ import { SectionComponent } from 'src/app/common/components/blank/section/sectio
 import { NavModel } from 'src/app/common/components/blank/models/nav.model';
 import { ReportModel } from './models/report.model';
 import { ReportService } from './services/report.service';
+import { PaginationResultModel } from 'src/app/common/models/pagination-result.model';
 
 @Component({
   selector: 'app-reports',
@@ -27,10 +28,13 @@ export class ReportsComponent implements OnInit, OnDestroy {
     },
   ];
 
-  reports: ReportModel[] = [];
+  result:PaginationResultModel<ReportModel[]> = new PaginationResultModel<ReportModel[]>
 
   count: number = 0;
   interval: any;
+  pageNumber :number =1;
+  pageSize:number=5;
+  pageNumbers: number[]= [];
 
   constructor(private _report: ReportService) {}
 
@@ -43,15 +47,24 @@ export class ReportsComponent implements OnInit, OnDestroy {
     this.interval = setInterval(() => {
       if (this.count < 5) {
         this.count++;
-        this.getAll();
+        this.getAll(this.pageNumber);
       } else {
         clearInterval(this.interval);
       }
     }, 5000);
   }
 
-  getAll() {
-    this._report.getAll((res) => (this.reports = res));
+  getAll(pageNumber:number=1) {
+    this.pageNumber = pageNumber;
+    this._report.getAll(this.pageNumber,this.pageSize,res =>{
+      this.result = res
+      this.pageNumbers =[]
+      for (let i = 0; i < res.totalPages; i++) {
+       this.pageNumbers.push(i+1);
+        
+      }
+    });
+    
   }
 
   changeSpanClassByStatus(status: boolean) {
